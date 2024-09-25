526fa0e4d0b858b21e3264105c5ff521f254a37c
Vazcuus <levkonst12@gmail.com>
2024-09-25 20:26:29 +0700

    try release 8

--- build/lab2/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: Y:/Programming/c-_lab2
-# Build directory: Y:/Programming/c-_lab2/build/lab2
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
