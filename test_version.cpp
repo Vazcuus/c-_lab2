eee8cfc92e353fcdb55647934e7cda98be267018
Vazcuus <levkonst12@gmail.com>
2024-10-05 11:48:17 +0700

    Check tests

--- test_version.cpp
+++ test_version.cpp
@@ -8,7 +8,7 @@ BOOST_AUTO_TEST_SUITE(test_version)
 
 BOOST_AUTO_TEST_CASE(test_valid_version)
 {
-    std::vector<int> = {12, 14, 15, 17, 18, 20};
+    std::vector<int> arr_test = {12, 14, 15, 17, 18, 20};
     BOOST_CHECK(LexiSort(arr_test[0], arr_test[1]) > 0);
     BOOST_CHECK(LexiSort(arr_test[2], arr_test[3]) > 0);
     BOOST_CHECK(LexiSort(arr_test[4], arr_test[5]) > 0);
