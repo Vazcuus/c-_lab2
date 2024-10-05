91ab445455bdf50fc96afb1eba901df792876763
Vazcuus <levkonst12@gmail.com>
2024-10-05 11:42:39 +0700

    Check tests

--- test_version.cpp
+++ test_version.cpp
@@ -8,9 +8,10 @@ BOOST_AUTO_TEST_SUITE(test_version)
 
 BOOST_AUTO_TEST_CASE(test_valid_version)
 {
-    BOOST_CHECK(LexiSort(12, 9) > 0);
-    BOOST_CHECK(LexiSort(20, 23) > 0);
-    BOOST_CHECK(LexiSort(52, 55) > 0);
+    int *arr_test = new int[6]{12, 14, 15, 17, 18, 20};
+    BOOST_CHECK(LexiSort(arr_test[0], arr_test[1]) > 0);
+    BOOST_CHECK(LexiSort(arr_test[2], arr_test[3]) > 0);
+    BOOST_CHECK(LexiSort(arr_test[4], arr_test[5]) > 0);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
